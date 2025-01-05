dc39810f090ce812898678b48796962cff9605b4
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-01-05 16:31:14 -0300

    Primeiro Commit

---
+++ src/sistema/iphone/aprelho/AparelhoTelefonico.java
@@ -0,0 +1,26 @@
+package sistema.iphone.aprelho;
+
+public class AparelhoTelefonico {
+	
+	
+	static void ligar(String numero) {
+	
+		System.out.println(" Ligar ");
+		
+	}
+	
+	
+	static void atender () {
+		
+		System.out.println(" Atendendo ");
+		
+	}
+	
+	static void iniciarCorreioDeVoz() {
+		
+		System.out.println(" Gravando Menssagem de Voz ");
+		
+	}
+	
+	
+}
---
+++ src/sistema/iphone/navegador/NavedorDeInternet.java
@@ -0,0 +1,28 @@
+package sistema.iphone.navegador;
+
+public class NavedorDeInternet {
+	
+	static void exibirPagina(String url) {
+		
+		System.out.println(" URL ");
+		
+	}
+	
+	
+	static void adicionarNovaAba() {
+		
+		System.out.println(" Adicionando Nova ABA ");
+	}
+	
+	
+	static void atualizarPagina() {
+		
+		System.out.println(" Atualizando Pagina ");
+		
+		
+	}
+	
+	
+	
+}
+
